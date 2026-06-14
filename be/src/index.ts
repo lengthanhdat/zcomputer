e0970e760c6bb98c923d3391eb4b9a5eb82416f1
huytruong <huy12094@gmail.com>
2026-06-14 19:59:22 +0700

    Fix CORS và sửa lỗi biến môi trường bị sai tên

--- be/src/index.ts
+++ be/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.50.213:3000'],
+    origin: true,
     credentials: true
   }
 });
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/zcomputer';
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.50.213:3000'],
+  origin: true,
   credentials: true
 }));
 app.use(compression()); 
